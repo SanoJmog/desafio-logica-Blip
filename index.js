1a54cb103665fbfb7b4fed6a450745000969a44c
SanoJmog <sano.jmog@gmail.com>
2026-01-18 18:40:54 -0300

    Finalizado!

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-console.log("Olá Mundão!")
+let characterName = "SanoJmog"
+let expCharacter = 10500
 
-console.log("Teste de Att.")
+if(expCharacter <= 1000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Ferro." )
+}
+else if(expCharacter > 1000 && expCharacter <= 2000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Bronze.")
+}
+else if(expCharacter > 2001 && expCharacter <= 5000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Prata.")
+}
+else if(expCharacter > 5001 && expCharacter <= 7000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Ouro.")
+}
+else if(expCharacter > 7001 && expCharacter <= 8000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Platina.")
+}
+else if(expCharacter > 8001 && expCharacter <= 9000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Ascendente.")
+}
+else if(expCharacter > 9001 && expCharacter <= 10000) {
+    console.log("O Herói de nome " + characterName + ", está no nível Imortal.")
+}
+
+else {
+    console.log("O Herói de nome " + characterName + ", é um MONSTRO RADIANTE.")
+}
+
